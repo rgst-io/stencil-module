c8255b7a9d8bbbd98643da8de3da5be1429e303e
Jared Allard <jared@rgst.io>
2026-03-28 09:38:16 -0700

    chore(deps): stencil-golang v3.0.1

--- stub.go
+++ stub.go
@@ -1,4 +1,3 @@
-// Package main is a stub package to ensure go commands do not fail.
-// Eventually, the release logic will be broken out so we no longer need
-// this.
+// Stub Go file to ensure go commands do not fail. Eventually, the
+// release logic will be broken out so we no longer need this.
 package main
